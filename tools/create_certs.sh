22fa76821df7b61a9a0efea3cd1798e4d0106484
Colby Williams <ColbyLWilliams@gmail.com>
2021-03-25 18:13:24 -0500

    Update create_certs.sh

--- tools/create_certs.sh
+++ tools/create_certs.sh
@@ -32,10 +32,6 @@ Options:
 
   -x  Signing Certificate name in KeyVault. Defaults to SignCertificate
 
-  -c  SSL Certificate name in KeyVault. Defaults to SSLCertificate
-
-  -u  Hostname of the Gateway for the SSL Certificate.
-
 Examples:
 
     $ create_cert.sh -v mykeyvault -x SignCert -c SSLCert -u mydomain.com
@@ -49,15 +45,13 @@ if (($# == 0)); then
 fi
 
 signCertificateName="SignCertificate"
-sslCertificateName="SSLCertificate"
+# sslCertificateName="SSLCertificate"
 
 # get arg values
-while getopts ":hv:x:c:u:" opt; do
+while getopts ":hv:x:" opt; do
     case $opt in
         v)  vaultName=$OPTARG;;
         x)  signCertificateName=$OPTARG;;
-        c)  sslCertificateName=$OPTARG;;
-        u)  sslCertHostName=$OPTARG;;
         h)  echo "$helpText" >&2; exit 0;;
         \?) echo "    Invalid option -$OPTARG $helpText" >&2; exit 1;;
         :)  echo "    Option -$OPTARG requires an argument $helpText." >&2; exit 1;;
@@ -68,9 +62,6 @@ done
 echo "Script variables:" >> $logfile
 echo "  vaultName: $vaultName" >> $logfile
 echo "  signCertificateName: $signCertificateName" >> $logfile
-echo "  sslCertificateName: $sslCertificateName" >> $logfile
-echo "  sslCertHostName: $sslCertHostName" >> $logfile
-
 
 # check for the azure cli
 if ! [ -x "$(command -v az)" ]; then
@@ -113,38 +104,6 @@ signCertPolicy='{
     }
 }'
 
-sslCertPolicy='{
-    "issuerParameters": {
-        "name": "Self"
-    },
-    "keyProperties": {
-        "exportable": true,
-        "keySize": 2048,
-        "keyType": "RSA",
-        "reuseKey": true
-    },
-    "lifetimeActions": [
-        {
-            "action": { "actionType": "AutoRenew" },
-            "trigger": { "daysBeforeExpiry": 90 }
-        }
-    ],
-    "secretProperties": {
-        "contentType": "application/x-pkcs12"
-    },
-    "x509CertificateProperties": {
-        "ekus": [ "1.3.6.1.5.5.7.3.1", "1.3.6.1.5.5.7.3.2" ],
-        "keyUsage": [ "digitalSignature", "keyEncipherment" ],
-        "subject": "'"CN=$sslCertHostName"'",
-        "subjectAlternativeNames": {
-            "dnsNames": [
-                "'"$sslCertHostName"'"
-            ]
-        },
-        "validityInMonths": 12
-    }
-}'
-
 echo "Getting signing certificate" >> $logfile
 signCert=$( az keyvault certificate show --vault-name $vaultName -n $signCertificateName )
 signCertResult=$?
@@ -172,42 +131,10 @@ signCertName=$( echo $signCert | jq -r '.name' )
 echo "Getting secret id for signing certificate" >> $logfile
 signCertSid=$( echo $signCert | jq -r '.sid' )
 
-
-echo "Getting ssl certificate" >> $logfile
-sslCert=$( az keyvault certificate show --vault-name $vaultName -n $sslCertificateName )
-sslCertResult=$?
-
-if [ $sslCertResult -ne 0 ]; then
-
-    echo "SSL certificate not found" >> $logfile
-
-    echo "Using ssl certificate policy" >> $logfile
-    echo "$sslCertPolicy" >> $logfile
-
-    echo "Creating new ssl certificate" >> $logfile
-    az keyvault certificate create --vault-name $vaultName -n $sslCertificateName -p "$sslCertPolicy" >> $logfile
-
-    echo "Getting ssl certificate details" >> $logfile
-    sslCert=$( az keyvault certificate show --vault-name $vaultName -n $sslCertificateName )
-fi
-
-echo "Getting id for ssl certificate" >> $logfile
-sslCertId=$( echo $sslCert | jq -r '.id' )
-
-echo "Getting secret name for ssl certificate" >> $logfile
-sslCertName=$( echo $sslCert | jq -r '.name' )
-
-echo "Getting secret id for ssl certificate" >> $logfile
-sslCertSid=$( echo $sslCert | jq -r '.sid' )
-
 outputJson='{
     "signCertificate": {
         "name": "'"$signCertName"'",
         "sid": "'"$signCertSid"'"
-    },
-    "sslCertificate": {
-        "name": "'"$sslCertName"'",
-        "sid": "'"$sslCertSid"'"
     }
 }'
 echo "Setting output json:" >> $logfile
