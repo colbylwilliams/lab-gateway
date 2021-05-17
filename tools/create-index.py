e772e78ab43b2f903f7a0dd482538dadbe2438f4
Colby Williams <ColbyLWilliams@gmail.com>
2021-05-17 16:51:56 -0500

    test +release

--- tools/create-index.py
+++ tools/create-index.py
@@ -1,5 +1,7 @@
+import os
 import json
 import argparse
+import subprocess
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
@@ -11,7 +13,47 @@ version = args.version.lower()
 if version[:1].isdigit():
     version = 'v' + version
 
+templates = []
+
+arm_dir = '{}/{}'.format(Path.cwd(), 'tools/tmp/arm')
+
+with os.scandir(Path.cwd() / 'arm/gateway') as s:
+    for f in s:
+        if f.name.endswith('.bicep') and f.is_file():
+            name = f.name.rsplit('.bicep', 1)[0]
+            arm_name = '{}.json'.format(name)
+            templates.append({
+                'name': name,
+                'bicep': {
+                    'name': f.name,
+                    'path': f.path,
+                },
+                'arm': {
+                    'name': arm_name,
+                    'path': '{}/{}'.format(arm_dir, arm_name)
+                }
+            })
+
+for t in templates:
+    print('Compiling template: {}'.format(t['name']))
+    subprocess.run(['az', 'bicep', 'build', '-f', t['bicep']['path'], '--outfile', t['arm']['path']])
+
+print("::set-output name=templates::{}".format(json.dumps(templates)))
+
 index = {}
+assets = []
+
+index['arm'] = {}
+
+for t in templates:
+    assets.append({'name': t['arm']['name'], 'path': t['arm']['path']})
+    index['arm'][t['name']] = {
+        'name': t['name'],
+        'version': '{}'.format(version),
+        'deployUrl': 'https://github.com/colbylwilliams/lab-gateway/releases/download/{}/{}'.format(version, t['arm']['name'])
+    }
+
+print("::set-output name=assets::{}".format(json.dumps(assets)))
 
 index['gateway'] = {
     'version': '{}'.format(version),
@@ -26,4 +68,9 @@ index['lab'] = {
 }
 
 with open(Path.cwd() / 'index.json', 'w') as f:
+    # with open(Path.cwd() / 'tools/tmp/index.json', 'w') as f:
     json.dump(index, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+with open(Path.cwd() / 'assets.json', 'w') as f:
+    # with open(Path.cwd() / 'tools/tmp/assets.json', 'w') as f:
+    json.dump(assets, f, ensure_ascii=False, indent=4, sort_keys=True)
