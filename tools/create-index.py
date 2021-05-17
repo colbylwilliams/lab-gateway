62909cce499c105444b0a64900f9ee14d0d4f23d
Colby Williams <ColbyLWilliams@gmail.com>
2021-05-17 18:37:36 -0500

    test +release

--- tools/create-index.py
+++ tools/create-index.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import subprocess
 from pathlib import Path
+from re import search
 
 parser = argparse.ArgumentParser()
 parser.add_argument('version', help='version number string')
@@ -13,45 +14,11 @@ version = args.version.lower()
 if version[:1].isdigit():
     version = 'v' + version
 
-# templates = []
-
-# arm_dir = '{}/{}'.format(Path.cwd(), 'assets/arm')
-# arm_dir = '{}/{}'.format(Path.cwd(), 'tools/tmp/arm')
-
-# with os.scandir(Path.cwd() / 'arm/gateway') as s:
-#     for f in s:
-#         if f.name.endswith('.bicep') and f.is_file():
-#             name = f.name.rsplit('.bicep', 1)[0]
-#             arm_name = '{}.json'.format(name)
-#             templates.append({
-#                 'name': name,
-#                 'bicep': {
-#                     'name': f.name,
-#                     'path': f.path,
-#                 },
-#                 'arm': {
-#                     'name': arm_name,
-#                     'path': '{}/{}'.format(arm_dir, arm_name)
-#                 }
-#             })
-
-# for t in templates:
-#     print('Compiling template: {}'.format(t['name']))
-#     subprocess.run(['az', 'bicep', 'build', '-f', t['bicep']['path'], '--outfile', t['arm']['path']])
-
 index = {}
-assets = []
-
 index['arm'] = {}
 index['artifacts'] = {}
 
-# for t in templates:
-#     assets.append({'name': t['arm']['name'], 'path': t['arm']['path']})
-#     index['arm'][t['name']] = {
-#         'name': t['name'],
-#         'version': '{}'.format(version),
-#         'url': 'https://github.com/colbylwilliams/lab-gateway/releases/download/{}/{}'.format(version, t['arm']['name'])
-#     }
+assets = []
 
 with os.scandir(Path.cwd() / 'assets/arm') as s:
     for f in s:
@@ -77,9 +44,7 @@ with os.scandir(Path.cwd() / 'assets/artifacts') as s:
 
 index['gateway'] = {
     'version': '{}'.format(version),
-    # 'deployUrl': 'https://github.com/colbylwilliams/lab-gateway/releases/download/{}/azuredeploy.json'.format(version),
     'zipUrl': 'https://github.com/colbylwilliams/lab-gateway/releases/download/{}/Gateway.zip'.format(version),
-    # 'scriptUrl': 'https://github.com/colbylwilliams/lab-gateway/releases/download/{}/gateway.ps1'.format(version),
 }
 
 assets.append({'name': 'Gateway.zip', 'path': '{}/{}'.format(Path.cwd(), 'Gateway.zip')})
@@ -88,6 +53,17 @@ assets.append({'name': 'Gateway.zip', 'path': '{}/{}'.format(Path.cwd(), 'Gatewa
 #     'version': '{}'.format(version),
 #     'deployUrl': 'https://github.com/colbylwilliams/lab-gateway/releases/download/{}/azuredeploy.lab.json'.format(version),
 # }
+
+with open(Path(Path.cwd() / 'client/lab-gateway') / 'setup.py', 'r') as f:
+    for line in f:
+        if line.startswith('VERSION'):
+            txt = str(line).rstrip()
+            match = search(r'VERSION = [\'\"](.*)[\'\"]$', txt)
+            if match:
+                cli_ver = match.group(1)
+                cli_name = 'lab_gateway-{}-py2.py3-none-any.whl'.format(cli_ver)
+                # assets.append({'name': cli_name, 'path': '{}/dist/{}'.format(Path.cwd(), cli_name)})
+                print("::set-output name=version::{}".format(cli_ver))
 
 
 with open(Path.cwd() / 'index.json', 'w') as f:
