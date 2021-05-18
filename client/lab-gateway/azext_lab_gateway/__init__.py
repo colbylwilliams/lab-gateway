e18af4500b96aa9e24eef84948a7f135ab6f5baf
Colby Williams <ColbyLWilliams@gmail.com>
2021-05-18 14:38:11 -0500

    test +release

--- client/lab-gateway/azext_lab_gateway/__init__.py
+++ client/lab-gateway/azext_lab_gateway/__init__.py
@@ -4,28 +4,24 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.core import AzCommandsLoader
+from azure.cli.core.commands import CliCommandType
 
 from ._help import helps  # pylint: disable=unused-import
+from ._params import load_arguments
+from .commands import load_command_table
 
 
 class LabGatewayCommandsLoader(AzCommandsLoader):
 
     def __init__(self, cli_ctx=None):
-        from azure.cli.core.commands import CliCommandType
-        from ._client_factory import resource_client_factory
-        lab_gateway_custom = CliCommandType(
-            operations_tmpl='azext_lab_gateway.custom#{}',
-            client_factory=resource_client_factory)
-        super(LabGatewayCommandsLoader, self).__init__(
-            cli_ctx=cli_ctx, custom_command_type=lab_gateway_custom)
+        lab_gateway_custom = CliCommandType(operations_tmpl='azext_lab_gateway.custom#{}')
+        super().__init__(cli_ctx=cli_ctx, custom_command_type=lab_gateway_custom)
 
     def load_command_table(self, args):
-        from .commands import load_command_table
         load_command_table(self, args)
         return self.command_table
 
     def load_arguments(self, command):
-        from ._params import load_arguments
         load_arguments(self, command)
 
 
